6ebc9ea398ea13adf0a7a1b2cccc5d23780d9a4d
nzaih1999 <>
2022-09-22 12:21:17 +0300

    finished event bubling

--- js/3_2_project_files/js_sandbox/js_sandbox/app.js
+++ js/3_2_project_files/js_sandbox/js_sandbox/app.js
@@ -127,6 +127,8 @@ taskInput.value = ""
 //taskInput.addEventListener('input' , runEvent)
 
 //form.addEventListener('submit', runEvent)
+
+// change
 const select = document.querySelector('select')
 
 select.addEventListener('change', runEvent)
@@ -137,4 +139,33 @@ function runEvent (e){
     heading.style.backgroundColor = 'yellow'
 }
 
-//change
+//event bubling
+// document.querySelector('.card-title').addEventListener('click', ()=>{
+//     console.log('card title')
+// })
+// document.querySelector('.card-content').addEventListener('click', ()=>{
+//     console.log('card content')
+// })
+// document.querySelector('.card').addEventListener('click', ()=>{
+//     console.log('card')
+// })
+// document.querySelector('.col').addEventListener('click', ()=>{
+//     console.log('col')
+// })
+
+//Event delegation
+
+// const delItem = document.querySelector('.delete-item')
+
+// delItem.addEventListener('click', deleteItem)
+document.body.addEventListener('click', deleteItem)
+
+function deleteItem (e){
+    
+   
+//    if(e.target.parentElement.className ==='delete-item secondary-content'){
+//   console.log('deleted')
+if(e.target.parentElement.classList.contains('delete-item')){
+    e.target.parentElement.parentElement.remove()
+   }
+}
